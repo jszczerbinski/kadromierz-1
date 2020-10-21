4a03d25113ffb4de84a7f4a1634351ce4eabb918
Beata Żurowska <beatawroc@gmail.com>
2020-10-21 02:23:07 +0200

    Updates

---
+++ precache-manifest.5fe46256803771a573cab92b188736b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f1ed1d432ca942138656faa3df096f",
+    "url": "/kadromierz/index.html"
+  },
+  {
+    "revision": "c5724fb400a129fc5cd7",
+    "url": "/kadromierz/static/css/main.4b24c879.chunk.css"
+  },
+  {
+    "revision": "6922ed5f070271c5696e",
+    "url": "/kadromierz/static/js/2.a39bded0.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/kadromierz/static/js/2.a39bded0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5724fb400a129fc5cd7",
+    "url": "/kadromierz/static/js/main.e52fd3a1.chunk.js"
+  },
+  {
+    "revision": "c0d4db6af90fd3a330b6",
+    "url": "/kadromierz/static/js/runtime-main.e729c9b0.js"
+  }
+]);
